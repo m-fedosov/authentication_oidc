7830fb2220eb2b324fd54eaa4293d24dad41b4a5
m-fedosov <fedosovmichael@gmail.com>
2025-05-12 12:31:48 +0300

    Initial commit

---
+++ src/main/java/com/example/oidc/config/SecurityConfig.java
@@ -0,0 +1,78 @@
+package com.example.oidc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .tokenEndpoint(tokenEndpoint ->
+                                tokenEndpoint
+                                        .accessTokenResponseClient(tokenResponseClient())
+                        )
+                        .defaultSuccessUrl("/hello", true)
+                        .userInfoEndpoint((userInfo) -> userInfo
+                                .userAuthoritiesMapper(grantedAuthoritiesMapper())
+                        )
+                )
+                .build();
+    }
+
+    @Bean
+    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> tokenResponseClient() {
+        return request -> {
+            System.out.println("CODE: " + request.getAuthorizationExchange().getAuthorizationResponse().getCode());
+            System.out.println("Client ID: " + request.getClientRegistration().getClientId());
+
+            DefaultAuthorizationCodeTokenResponseClient delegate = new DefaultAuthorizationCodeTokenResponseClient();
+            return delegate.getTokenResponse(request);
+        };
+    }
+
+    private GrantedAuthoritiesMapper grantedAuthoritiesMapper() {
+        return (authorities) -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+            authorities.forEach((authority) -> {
+                GrantedAuthority mappedAuthority;
+
+                if (authority instanceof OidcUserAuthority userAuthority) {
+                    mappedAuthority = new OidcUserAuthority(
+                            "OIDC_USER", userAuthority.getIdToken(), userAuthority.getUserInfo());
+                    System.out.println(userAuthority.getIdToken());
+                    System.out.println(userAuthority.getUserInfo());
+                } else if (authority instanceof OAuth2UserAuthority userAuthority) {
+                    mappedAuthority = new OAuth2UserAuthority(
+                            "OAUTH2_USER", userAuthority.getAttributes());
+                } else {
+                    mappedAuthority = authority;
+                }
+
+                mappedAuthorities.add(mappedAuthority);
+            });
+
+            return mappedAuthorities;
+        };
+    }
+}
